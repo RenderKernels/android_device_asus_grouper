0b2b8a6fa68cbb57ed61a7b998ca4b5de6655fd5
Snuzzo <jrracinfan@gmail.com>
2015-09-20 12:57:33 -0400

    Use CORE_COUNT variable / Do not override build targets

--- makekernel.sh
+++ makekernel.sh
@@ -2,6 +2,6 @@ cd ${4}
 export ${3}
 export ${2}
 make ${1}
-make -j9
+make -j$CORE_COUNT
 mkdir -p $OUT_DIR
 mv ./arch/arm/boot/zImage $OUT_DIR
--- render.mk
+++ render.mk
@@ -2,21 +2,5 @@ KERNEL_BUILD_SCRIPT := device/asus/nexus7/makekernel.sh
 PRODUCT_NAME := render_nexus7
 PRODUCT_DEFCONFIG := tegra3_android_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/asus/nexus7
-CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.1/bin/arm-eabi-
+CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
 ARCH := arm
-
-.PHONY: kernel
-kernel: nothing
-	$(hide) $(KERNEL_BUILD_SCRIPT) \
-			$(PRODUCT_DEFCONFIG) \
-			ARCH=$(ARCH) \
-			CROSS_COMPILE=$(CROSS_COMPILE) \
-			$(PRODUCT_KERNEL_SOURCE)
-			
-.PHONY: kernelclean
-kernelclean: nothing
-	$(shell cd $(PRODUCT_KERNEL_SOURCE) && make mrproper)
-
-.PHONY: kernelclobber
-kernelclobber: kernelclean
-	$(shell rm -rf $OUT_DIR/*)
