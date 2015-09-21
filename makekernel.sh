a0ea913af926e70ccd75312ed04301c0ce19a625
Snuzzo <jrracinfan@gmail.com>
2015-09-21 11:35:56 -0400

    Remove helper script. Use build target to compile.

--- makekernel.sh
+++
@@ -1,7 +0,0 @@
-cd ${4}
-export ${3}
-export ${2}
-make ${1}
-make -j$CORE_COUNT
-mkdir -p $OUT_DIR
-mv ./arch/arm/boot/zImage $OUT_DIR
--- render.mk
+++ render.mk
@@ -1,6 +1,6 @@
-KERNEL_BUILD_SCRIPT := device/asus/nexus7/makekernel.sh
 PRODUCT_NAME := render_nexus7
 PRODUCT_DEFCONFIG := tegra3_android_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/asus/nexus7
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
 ARCH := arm
+ZIMAGE := arch/arm/boot/zImage
