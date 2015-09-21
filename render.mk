c092c47b6c3562e81f1da964f0f64312d0da5737
Snuzzo <jrracinfan@gmail.com>
2015-09-21 11:42:25 -0400

    Track/Build with Uber 5.2 arm-eabi

--- render.mk
+++ render.mk
@@ -1,6 +1,6 @@
 PRODUCT_NAME := render_nexus7
 PRODUCT_DEFCONFIG := tegra3_android_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/asus/nexus7
-CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
+CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
 ZIMAGE := arch/arm/boot/zImage
