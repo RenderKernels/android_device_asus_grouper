b9cddb35d7e5bc4d4ec49ffeab2152236126a1c1
Snuzzo <jrracinfan@gmail.com>
2015-09-22 11:47:42 -0400

    Small clean-up

--- render.mk
+++ render.mk
@@ -4,3 +4,5 @@ PRODUCT_KERNEL_SOURCE := kernel/asus/grouper
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
 ZIMAGE := arch/arm/boot/zImage
+
+include vendor/rendor/configs/common.mk
