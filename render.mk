f18ead8f950d6b4c9055dafe91ba8cd57862fc3c
Snuzzo <jrracinfan@gmail.com>
2015-11-01 12:49:01 -0500

    ARCH: Use default define

--- render.mk
+++ render.mk
@@ -4,7 +4,6 @@ PRODUCT_NAME := render_grouper
 PRODUCT_DEFCONFIG := tegra3_android_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/asus/grouper
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
-ARCH := arm
 ZIP_FILES_DIR := device/asus/grouper/zip_files
 
 #Override with my custom versioning -Snuzzo was here
